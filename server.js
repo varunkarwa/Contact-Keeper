d71aa75e864f49fe4b8872e383917740ac4fb963
varunkarwa <66462469+varunkarwa@users.noreply.github.com>
2021-01-10 16:08:14 +0530

    Backend Changes Commit

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ msg: 'Welocme to the Contact Keeper API...'}));
+//Connect Database
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => 
+    res.json({ msg: 'Welcome to the ContactKeeper API...'})
+);
+
+//Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 
